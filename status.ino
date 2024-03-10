9fea42b276583c3fc270f18356a1f25f966804ff
John Petrangelo <john@petrangelo.org>
2024-03-10 00:49:25 -0500

    Moved getStatus into it's own file

---
+++ status.ino
@@ -0,0 +1,76 @@
+#include <memory>
+#include <string>
+
+#include <ArduinoJson.h>
+#include <ESP8266WebServer.h>
+#include <LittleFS.h>
+
+//#include "src/utils.h"
+
+String getStatus() {
+  StaticJsonDocument<512> doc;
+  doc["time"] = millis() / 1000.0;
+
+  JsonObject memory = doc.createNestedObject("memory");
+  memory["freeHeapBytes"] = ESP.getFreeHeap();
+
+  JsonObject fs = doc.createNestedObject("filesystem");
+  FSInfo fsInfo;
+  bool gotInfo = LittleFS.info(fsInfo);
+  if (gotInfo) {
+    fs["totalBytes"] = fsInfo.totalBytes;
+    fs["usedBytes"] = fsInfo.usedBytes;
+    fs["blockSize"] = fsInfo.blockSize;
+    fs["pageSize"] = fsInfo.pageSize;
+    fs["maxOpenFiles"] = fsInfo.maxOpenFiles;
+    fs["maxPathLength"] = fsInfo.maxPathLength;
+  }
+
+  JsonObject network = doc.createNestedObject("network");
+  network["hostname"] = hostname + ".local";
+  network["wifiMACAddress"] = WiFi.macAddress();
+  network["ipAddress"] = WiFi.localIP().toString();
+  network["mode"] = "unknown";
+  switch(WiFi.getMode()) {
+    case WIFI_OFF:
+      network["mode"] = "WIFI_OFF";
+      break;
+    case WIFI_STA:
+      network["mode"] = "WIFI_STA";
+      break;
+    case WIFI_AP:
+      network["mode"] = "WIFI_AP";
+      break;
+    case WIFI_AP_STA:
+      network["mode"] = "WIFI_AP_STA";
+      break;
+    default:
+      network["mode"] = String(WiFi.getMode());
+      break;
+  }
+  network["phyMode"] = "unknown";
+  switch(WiFi.getPhyMode()) {
+    case WIFI_PHY_MODE_11B:
+      network["phyMode"] = "WIFI_PHY_MODE_11B";
+      break;
+    case WIFI_PHY_MODE_11G:
+      network["phyMode"] = "WIFI_PHY_MODE_11G";
+      break;
+    case WIFI_PHY_MODE_11N:
+      network["phyMode"] = "WIFI_PHY_MODE_11N";
+      break;
+    default:
+      network["phyMode"] = String(WiFi.getPhyMode());
+      break;
+  }
+  network["softAPssid"] = hostname;
+  network["softAPStationNum"] = WiFi.softAPgetStationNum();
+  network["softAPIP"] = WiFi.softAPIP();
+  network["softAPmacAddress"] = WiFi.softAPmacAddress();
+
+  String output;
+  serializeJsonPretty(doc, output);
+  output += "\n";
+
+  return output;
+}
