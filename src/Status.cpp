f80b6ee421c3bfe9ba82ced956101193c122ae1a
John Petrangelo <john@petrangelo.org>
2024-03-18 00:03:59 -0400

    Organized system status into its own function

--- src/Status.cpp
+++ src/Status.cpp
@@ -5,8 +5,7 @@
 #include "Network.h"
 #include "Status.h"
 
-String getStatus() {
-  JsonDocument doc;
+static void systemGetStatus(JsonObject obj) {
   auto now = millis();
   auto days = now / (24*60*60*1000);
   now %= 24*60*60*1000;
@@ -18,22 +17,25 @@ String getStatus() {
   auto milliSecs = now % 1000;
 
   char buffer[32];
-  snprintf(buffer, sizeof(buffer),"%lud %luh %lum %lu.%0.3lus", days, hours, mins, secs, milliSecs);
-  doc["time"] = buffer;
+  sprintf(buffer,"%lud %luh %lum %lu.%0.3lus", days, hours, mins, secs, milliSecs);
+  obj["time"] = String(buffer);
 
+  obj["freeHeapBytes"] = EspClass::getFreeHeap();
+  obj["heapFragmentation"] = EspClass::getHeapFragmentation();
+  obj["maxFreeBlockSize"] = EspClass::getMaxFreeBlockSize();
+  obj["sketchUsedBytes"] = EspClass::getSketchSize();
+  obj["sketchFreeBytes"] = EspClass::getFreeSketchSpace();
+  obj["lastResetReason"] = EspClass::getResetReason();
+  obj["cpuFreqMHz"] = EspClass::getCpuFreqMHz();
+  obj["flashChipSize"] = EspClass::getFlashChipSize();
+  obj["realFlashChipSize"] = EspClass::getFlashChipRealSize();
+}
+
+String getStatus() {
+  JsonDocument doc;
+
+  systemGetStatus(doc["system"].to<JsonObject>());
   Network::getRenderer()->getStatus(doc["renderer"].to<JsonObject>());
-
-  JsonObject system = doc["system"].to<JsonObject>();
-  system["freeHeapBytes"] = EspClass::getFreeHeap();
-  system["heapFragmentation"] = EspClass::getHeapFragmentation();
-  system["maxFreeBlockSize"] = EspClass::getMaxFreeBlockSize();
-  system["sketchUsedBytes"] = EspClass::getSketchSize();
-  system["sketchFreeBytes"] = EspClass::getFreeSketchSpace();
-  system["lastResetReason"] = EspClass::getResetReason();
-  system["cpuFreqMHz"] = EspClass::getCpuFreqMHz();
-  system["flashChipSize"] = EspClass::getFlashChipSize();
-  system["realFlashChipSize"] = EspClass::getFlashChipRealSize();
-
   Filesystem::getStatus(doc["filesystem"].to<JsonObject>());
   Network::getStatus(doc["network"].to<JsonObject>());
 
