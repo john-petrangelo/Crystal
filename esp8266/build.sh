eff2748b36a54dbf3986c23c8b3753f38ffcaf13
John Petrangelo <john@petrangelo.org>
2024-08-10 00:32:22 -0400

    Improve use of quotes and use bash for better portability

--- esp8266/build.sh
+++ esp8266/build.sh
@@ -1,13 +1,15 @@
+#!/bin/bash
+
 # A short script that builds the image for ESP8266 using the arduino-cli tool
 
 # Set up the fully-qualified board name
 PLATFORM=esp8266
 BOARD=esp8266
 VARIANT=generic
-FQBN=$PLATFORM:$BOARD:$VARIANT
+FQBN="$PLATFORM:$BOARD:$VARIANT"
 
 # Add lib directory to include path
-BUILD_PROPERTY=build.extra_flags="-I../lib"
+BUILD_PROPERTY='build.extra_flags="-I../lib"'
 
 # Build the image
 arduino-cli compile --fqbn $FQBN --build-property "$BUILD_PROPERTY" -v esp8266.ino
