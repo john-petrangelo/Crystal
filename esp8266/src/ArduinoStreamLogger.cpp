acd22f57d5f131697eee1bc0c2a0c23cfdcc685e
John Petrangelo <john@petrangelo.org>
2024-08-15 00:00:33 -0400

    Cleanup, removed leftover cruft

--- esp8266/src/ArduinoStreamLogger.cpp
+++ esp8266/src/ArduinoStreamLogger.cpp
@@ -1,11 +1,4 @@
-#include <HardwareSerial.h>
-
-#include <lumos-arduino/defs.h>
-#include <lumos-arduino/ILogger.h>
-
 #include "ArduinoStreamLogger.h"
-
-ILogger *logger;
 
 void ArduinoStreamLogger::log(char const *msg) {
   stream->print(msg);
