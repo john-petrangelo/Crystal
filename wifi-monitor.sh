050c1f84a8969affe3e6020bfc9412dc6f86b6cd
John Petrangelo <john@petrangelo.org>
2024-08-25 00:16:01 -0400

    Minor improvements, improved comments and quoted variables

--- wifi-monitor.sh
+++ wifi-monitor.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Connects to port 8000 on e specified target host to listen for log messages.
+# Tries to reconnect if the connection is closed.
+
 # Default HOST value
 HOST_DEFAULT=192.168.4.165
 
@@ -9,6 +12,6 @@ HOST="${1:-$HOST_DEFAULT}"
 echo "Connecting to $HOST"
 while true
 do
-  nc $HOST 8000
-  echo "Disconnected from $HOST, retrying..."
+  nc "$HOST" 8000
+  echo "Disconnected from $HOST, attempting to reconnect..."
 done
