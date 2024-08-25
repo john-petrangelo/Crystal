dc9f14fb7a1ec1a8c8866425ee0baf5f7b1ceb5d
John Petrangelo <john@petrangelo.org>
2024-08-24 23:47:40 -0400

    Fixed default address typo

--- wifi-monitor.sh
+++ wifi-monitor.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 # Default HOST value
-HOST_DEFAULT="192.68.4.165"
+HOST_DEFAULT=192.168.4.165
 
 # Use the first command-line argument as HOST if provided, otherwise use the default
 HOST="${1:-$HOST_DEFAULT}"
 
+echo "Connecting to $HOST"
 while true
 do
   nc $HOST 8000
-  echo "Disconnected, retrying..."
+  echo "Disconnected from $HOST, retrying..."
 done
