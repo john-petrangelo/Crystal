3ec079aa6f554a0f345b4cf1ff0d968408dfd27d
John Petrangelo <john@petrangelo.org>
2024-11-22 22:25:57 -0500

    Render Dark Crystal by default at startup

--- raspberry-pi-pico/Crystal.cpp
+++ raspberry-pi-pico/Crystal.cpp
@@ -7,29 +7,15 @@
 #include "Network.h"
 #include "Renderer.h"
 #include "Status.h"
-#include "WS2812Strip.h"
 
 #include "Models/Crystal.h"
-#include "Models/Gradient.h"
-#include "Models/Map.h"
-#include "Models/Rotate.h"
 #include "Models/Solid.h"
-#include "Models/Sum.h"
 
-constexpr long logDurationIntervalMS = 10000;
+constexpr long logDurationIntervalMS = 60000;
 
 int main() {
   stdio_init_all();
   Logger::set(new ConsoleLogger());
-
-  // Initialize the renderer
-  Renderer *renderer = new RaspberryPiPico_Renderer(64);
-  renderer->render();
-
-  ModelPtr m1 = Gradient::make(RED, ORANGE, YELLOW, GREEN, BLUE, INDIGO, VIOLET, RED);
-  ModelPtr model = std::make_shared<Rotate>("RotatingRainbow", 0.3, m1);
-
-  renderer->setModel(model);
 
   if (cyw43_arch_init()) {
     logger << "Wi-Fi init failed" << std::endl;
@@ -38,15 +24,19 @@ int main() {
 
   logger << "Connecting to network" << std::endl;
   Network::setup();
-  Network::setRenderer(renderer);
 
-  auto strip = WS2812Strip(22, 64);
-
+  // Initialize the renderer
+  Renderer *renderer = new RaspberryPiPico_Renderer(64);
+  ModelPtr model = Crystal::make();
+  renderer->setModel(model);
+  renderer->render();
   auto lastUpdateMS = to_ms_since_boot(get_absolute_time());
+  Network::setRenderer(renderer);
 
   while (true) {
     auto const now_ms = to_ms_since_boot(get_absolute_time());
     renderer->render();
+
     Network::loop();
 
     if (now_ms - lastUpdateMS >= logDurationIntervalMS) {
