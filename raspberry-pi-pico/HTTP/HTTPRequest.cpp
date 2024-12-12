86ed7e7e5450a64cc6291742b3c404b8296461b8
John Petrangelo <john@petrangelo.org>
2024-12-11 23:37:35 -0500

    Improved formatting of HTTPRequest objects in log

--- raspberry-pi-pico/HTTP/HTTPRequest.cpp
+++ raspberry-pi-pico/HTTP/HTTPRequest.cpp
@@ -3,34 +3,37 @@
 #include "HTTPRequest.h"
 
 std::ostream& operator<<(std::ostream& os, const HTTPRequest& request) {
-    os << "HTTP Request - " << request.method << " " << request.path << std::endl;
+    os << "HTTP Request" << std::endl;
+    os << "  " << request.method << " " << request.path << std::endl;
 
     if (request.queryParams.empty()) {
-        os << "No query parameters" << std::endl;
+        os << "  No query parameters" << std::endl;
     } else {
-        os << "Query params:" << std::endl;
+        os << "  Query params:" << std::endl;
         for (const auto& param : request.queryParams) {
             os << "  " << param.first << ": " << param.second << std::endl;
         }
     }
 
     if (request.headers.empty()) {
-        os << "No headers" << std::endl;
+        os << "  No headers" << std::endl;
     } else {
-        os << request.headers.size() << " headers" << std::endl;
+        os << "  " << request.headers.size() << " headers" << std::endl;
 
-        // os << "Headers:" << std::endl;
+        // os << "  Headers:" << std::endl;
         // for (const auto& header : request.headers) {
         //     os << "  " << header.first << ": " << header.second << std::endl;
         // }
     }
 
     if (request.body.empty()) {
-        os << "No body" << std::endl;
+        os << "  No body" << std::endl;
     } else {
-        os << "Body: <<<" << std::endl;
-        os << request.body << std::endl;
-        os << ">>> END OF BODY" << std::endl;
+        os << "  Body is " << request.body.length() << " bytes" << std::endl;
+
+        // os << "  Body: <<<" << std::endl;
+        // os << request.body << std::endl;
+        // os << "  >>> END OF BODY" << std::endl;
     }
 
     return os;
