cfcea9f7a3bda919d82181a6d10cfac446cd6a14
John Petrangelo <john@petrangelo.org>
2025-02-17 00:21:10 -0500

    Add scan timeout

--- raspberry-pi-pico/Networking/Network.cpp
+++ raspberry-pi-pico/Networking/Network.cpp
@@ -289,25 +289,48 @@ void logScanResults(std::vector<Network::ScanResult> const &results) {
 }
 
 void Network::scanWiFi() {
-  cyw43_wifi_scan_options_t scan_options = {};
-  scan_options.ssid_len = 0; /*all*/
-  scan_options.scan_type = 0; /*active*/
-
   // Clear out any previous scan results
   scanResults.clear();
 
-  logger << "Starting Wi-Fi scan..." << std::endl;
+  // Need to be in station mode and not connected to a network to scan
   cyw43_arch_enable_sta_mode();
-  cyw43_wifi_scan(&cyw43_state, &scan_options, nullptr, scanWiFiCallback);
 
-  // TODO Add timeout for safety
+  // Start the scan
+  logger << "Starting Wi-Fi scan..." << std::endl;
+  cyw43_wifi_scan_options_t scan_options = {};
+  scan_options.ssid_len = 0; /*all*/
+  scan_options.scan_type = 0; /*active*/
+  auto const err = cyw43_wifi_scan(&cyw43_state, &scan_options, nullptr, scanWiFiCallback);
+  if (err) {
+    logger << "Wi-Fi scan failed: " << cyw43ErrStr(err) << std::endl;
+    return;
+  }
+
+  // Wait this long before giving up on the scan completing
+  auto constexpr SCAN_TIMEOUT_MS = 3000;
+
+  // Save the start time so we can calculate the elapsed time
+  auto const start_time_ms = to_ms_since_boot(get_absolute_time());
+
+  // Wait for the scan to complete or the timeout elapsed
   while (cyw43_wifi_scan_active(&cyw43_state)) {
+    // Give the network a chance to run
     cyw43_arch_poll();
+
+    // Check if the scan has timed out
+    auto const elapsed_time_ms = absolute_time_diff_us(start_time_ms, get_absolute_time()) / 1000;
+    if (elapsed_time_ms > SCAN_TIMEOUT_MS) {
+      logger << "Wi-Fi scan timed out after " << elapsed_time_ms << "ms" << std::endl;
+      break;
+    }
+
+    // Wait a short time and repeat
     sleep_ms(100);
   }
 
+  // Sort scan results by RSSI (strongest signal first)
   std::ranges::sort(scanResults,
-                    [](ScanResult const &a, ScanResult const &b) { return a.rssi > b.rssi; });
+      [](ScanResult const &a, ScanResult const &b) { return a.rssi > b.rssi; });
 
   logger << "Finished Wi-Fi scan" << std::endl;
 }
