7eb931eef257974213a213adbccc28a6290e20ac
John Petrangelo <john@petrangelo.org>
2025-03-26 21:38:40 -0400

    Minor refactoring

--- secrets.h
+++ secrets.h
@@ -6,5 +6,5 @@
 
 ////////////////////////////////////////////////////////////////////////////
 // Set the name and password for your WiFi network here.
-#define SECRET_SSID "BlueFi"
-#define SECRET_PASSWORD "takeoutmenus"
+#define SECRET_SSID "myssid"
+#define SECRET_PASSWORD "mypassword"
